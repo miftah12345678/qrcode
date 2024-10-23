1abc17c28b9d9e2e164eed3f8b33f7770fb16d6d
hamstore <165684120+miftah12345678@users.noreply.github.com>
2024-10-23 13:59:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-const wrapper = document.querySelector(".wrapper"),
-  qrInput = wrapper.querySelector(".form input"),
-  generateBtn = wrapper.querySelector(".form button"),
-  qrImg = wrapper.querySelector(".qr-code img");
-let preValue;
+const image = document.querySelector("img"),
+      input = document.querySelector("input"),
+      button = document.querySelector("button"),
+      api = `https://api.qrserver.com/v1/`,
+      api2 = `create-qr-code/?size=160x160&data=`;
 
-generateBtn.addEventListener("click", () => {
-  let qrValue = qrInput.value.trim();
-  if (!qrValue || preValue === qrValue) return;
-  preValue = qrValue;
-  generateBtn.innerText = "Generating QR Code...";
-  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-  qrImg.addEventListener("load", () => {
-    wrapper.classList.add("active");
-    generateBtn.innerText = "Generate QR Code";
-  });
-});
-
-qrInput.addEventListener("keyup", () => {
-  if (!qrInput.value.trim()) {
-    wrapper.classList.remove("active");
-    preValue = "";
-  }
+button.addEventListener("click", () => {
+    image.src = `${api}${api2}${input.value}`
 });
