b2eabad592ce383efa832cbb8a9e5d45588fd1ca
hamstore <165684120+miftah12345678@users.noreply.github.com>
2024-10-25 14:42:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const image = document.querySelector("img"),
-      input = document.querySelector("input"),
-      button = document.querySelector("button"),
-      api = `https://api.qrserver.com/v1/`,
-      api2 = `create-qr-code/?size=160x160&data=`;
-
-button.addEventListener("click", () => {
-    image.src = `${api}${api2}${input.value}`
+document.getElementById('generateLinkQR').addEventListener('click', function() {
+    const linkInput = document.getElementById('linkInput').value;
+    const qr = new QRious({
+        element: document.getElementById('linkQRCode'),
+        value: linkInput,
+        size: 200
+    });
 });
 
-// Render Hasilnya
-  outputImg.src = canvas.toDataURL();
-  outputImg.style.display = "block";
-  downloadButton.style.display = "inline-block";
-}
-
-downloadButton.addEventListener("click", () => {
-  const outputImg = document.getElementById("generatedImage");
-  const link = document.createElement("a");
-  link.href = outputImg.src;
-  link.download = "follow_ig_@ughbmm.png";
-  link.click();
+document.getElementById('generateImageQR').addEventListener('click', function() {
+    const imageInput = document.getElementById('imageInput').files[0];
+    if (imageInput) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const qr = new QRious({
+                element: document.getElementById('imageQRCode'),
+                value: event.target.result,
+                size: 200
+            });
+        };
+        reader.readAsDataURL(imageInput);
+    } else {
+        alert('Silakan pilih gambar terlebih dahulu.');
+    }
 });
